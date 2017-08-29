95f6d83037972231b91222daf2b4a687a816d947
Matt Barker <mjb485@cornell.edu>
2017-08-28 23:00:40 -0400

    End-to-end app refactoring for use in Big Red Hacks presentation

--- FeedMe/FeedViewController.swift
+++ FeedMe/FeedViewController.swift
@@ -9,8 +9,7 @@
 import UIKit
 import SafariServices
 
-class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UINavigationControllerDelegate,
-                            SFSafariViewControllerDelegate, UITextFieldDelegate {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UINavigationControllerDelegate, UITextFieldDelegate {
     
     @IBOutlet weak var newFeedTextField: UITextField!
     @IBOutlet weak var feedStatus: UILabel!
@@ -18,10 +17,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var tableView: UITableView!
     
     var delegate: NewFeedDelegate?
-    var feedArrayInFeedView = [RSSFeed]()
-    var singleFeedArray = [RSSItem]()
-    var newLink: URL?
-    var alertController: UIAlertController?
+    var feedArray = [RSSFeed]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +28,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         feedStatus.textColor = UIColor.darkText
         feedStatus.isHidden = true
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -44,129 +41,107 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return feedArrayInFeedView.count
+        return feedArray.count
     }
     
-    //set each RSSFeed info to appear in table for list of feeds
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Feed") as! FeedViewCell
-        cell.setFeed(feedArrayInFeedView[(indexPath as NSIndexPath).row])
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Feed")!
+        cell.textLabel?.text = feedArray[indexPath.row].title
+        cell.detailTextLabel?.text = feedArray[indexPath.row].link?.absoluteString
         return cell
     }
     
-    //allows table cells to be edited
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool { return true }
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) { }
+    // MARK: Cell Editing Functions
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
+    
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         
-        //remove feed from feedList
         let delete = UITableViewRowAction(style: .normal, title: "Delete") { action, index in
-            
-            self.feedArrayInFeedView.remove(at: (indexPath as NSIndexPath).row)
-            //prepareForSegue transfer more stable?
-            self.delegate?.didChangeFeedArray(self.feedArrayInFeedView)
-            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.fade)
+            self.feedArray.remove(at: indexPath.row)
+            self.delegate?.didChange(to: self.feedArray)
+            tableView.deleteRows(at: [indexPath], with: .fade)
             self.feedStatus.isHidden = false
             self.feedStatus.text = "Feed Removed!"
-            self.feedStatus.textColor = UIColor.green
+            self.feedStatus.textColor = .green
         }
-        delete.backgroundColor = UIColor.red
         
-        let rename = UITableViewRowAction(style: .normal, title: "Rename") { action, index in
-            
-            //create rename dialog box
-            self.alertController = UIAlertController(title: "Change Feed Name", message: nil, preferredStyle: .alert)
-            self.alertController!.addTextField( configurationHandler: {(textField: UITextField!) in
-                    textField.text = self.feedArrayInFeedView[(indexPath as NSIndexPath).row].customTitle ?? self.feedArrayInFeedView[(indexPath as NSIndexPath).row].title })
-            
-            let action = UIAlertAction(title: "Save", style: UIAlertActionStyle.default, handler: {[weak self] (paramAction:UIAlertAction!) in
-                    if let textFields = self!.alertController?.textFields {
-                        let theTextFields = textFields as [UITextField]
-                        if theTextFields[0].text != "" {
-                            self!.feedArrayInFeedView[(indexPath as NSIndexPath).row].customTitle = theTextFields[0].text
-                            self!.delegate?.didChangeFeedArray(self!.feedArrayInFeedView)
-                            tableView.reloadData()
-                        }
-                        self!.tableView.setEditing(false, animated: true)
-                        self!.feedStatus.isHidden = false
-                        self!.feedStatus.text = "Feed Renamed!"
-                        self!.feedStatus.textColor = UIColor.green
-                    }
-                })
-            
-            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: {[weak self] (paramAction:UIAlertAction!) in self!.tableView.setEditing(false, animated: true)})
-            self.alertController?.addAction(action)
-            self.alertController?.addAction(cancel)
-            self.automaticallyAdjustsScrollViewInsets = false
-            self.present(self.alertController!, animated: true, completion: nil)
-            
-        }
-        rename.backgroundColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 1.0)
-        return [delete, rename]
+        delete.backgroundColor = .red
+        
+        return [delete]
+        
     }
 
-    //checks if feed already exists in list
-    func checkDup(_ feed: RSSFeed, url: URL) -> Bool {
-        for everyHomePage in self.feedArrayInFeedView {
-            if everyHomePage.link == feed.link {
+    // Check if feed already exists in list
+    func checkDup(_ existingFeed: RSSFeed, url: URL) -> Bool {
+        for feed in feedArray {
+            if feed.link == existingFeed.link {
                 self.feedStatus.text = "Feed Already In List!"
-                self.feedStatus.textColor = UIColor.red
+                self.feedStatus.textColor = .red
                 return false
-                    }
+            }
         }
         return true
     }
     
     //parse and save new url feed
     @IBAction func saveFeed() {
+        
         if let validURL = URL(string: newFeedTextField.text!) {
+            
             let request: URLRequest = URLRequest(url: validURL)
-            RSSParser.parseFeedForRequest(request, callback: { (feed, error) in
+            
+            RSSParser.parseFeedForRequest(request) { (feed, error) in
+                
                 if feed != nil && self.checkDup(feed!, url: validURL) {
                     feed!.rawLink = validURL
-                    self.feedArrayInFeedView.append(feed!)
+                    self.feedArray.append(feed!)
                     self.tableView.reloadData()
-                    self.delegate?.didChangeFeedArray(self.feedArrayInFeedView)
+                    self.delegate?.didChange(to: self.feedArray)
                     self.feedStatus.text = "Feed Successfully Added!"
                     self.feedStatus.textColor = UIColor.green
-                        }
-                //nothing in textfield
+                }
+                
+                // nothing in textfield
                 if self.newFeedTextField.text == "" {
                     self.feedStatus.text = "Enter Something First!"
                     self.feedStatus.textColor = UIColor.red
-                        }
-                //not a valid url
+                }
+                
+                // not a valid url
                 if feed == nil {
                     self.feedStatus.text = "Invalid Feed"
                     self.feedStatus.textColor = UIColor.red
-                        }
+                }
+                
                 self.feedStatus.isHidden = false
                 self.newFeedTextField.text = ""
                 self.tableView.reloadData()
-            })
+                
+            }
         }
+        
         view.endEditing(true)
     }
     
-    //keyboard: return key fires saveFeed, dismisses keyboard (as well as through any other touch)
     func textFieldShouldReturn(_ newFeedTextField: UITextField) -> Bool {
         newFeedTextField.resignFirstResponder()
         saveFeed()
         return true
     }
     
-    //open feed homepage if tapped
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let link = feedArrayInFeedView[(indexPath as NSIndexPath).row].link
-        let svc = SFSafariViewController(url: link! as URL)
-        svc.delegate = self
-        self.present(svc, animated: true, completion: nil)
+        let feed = feedArray[indexPath.row]
+        let safariViewController = SFSafariViewController(url: feed.link!)
+        present(safariViewController, animated: true, completion: nil)
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
-    //dismiss SVC
-    func safariViewControllerDidFinish(_ controller: SFSafariViewController)
-    { controller.dismiss(animated: true, completion: nil) }
     
     @IBAction func unwindToFeedView(_ sender: UIStoryboardSegue) { }
     
--- FeedMe/TableViewController.swift
+++ FeedMe/TableViewController.swift
@@ -10,187 +10,148 @@ import UIKit
 import SafariServices
 
 protocol NewFeedDelegate {
-    func didChangeFeedArray(_ newFeedArray: [RSSFeed])
+    func didChange(to newFeedArray: [RSSFeed])
 }
 
 class TableViewController: UITableViewController, SFSafariViewControllerDelegate, NewFeedDelegate {
     
-    var feedArray = [RSSFeed]()
     var itemArray = [RSSItem]()
-    var customTitle: String?
+    var feedArray = [RSSFeed]()
     
-    //starter sites
-    var addressArray = [URL(string: "http://machash.com/feed/")!,
-                        URL(string: "http://rss.news.yahoo.com/rss/entertainment")!,
-                        URL(string: "http://feeds.nytimes.com/nyt/rss/Technology")!]
+    // Starter sites
+    var addressArray = [
+        
+        "http://machash.com/feed/",
+        "http://rss.news.yahoo.com/rss/entertainment",
+        "http://feeds.nytimes.com/nyt/rss/Technology",
+    
+    ]
     
     //NewFeedDelegate Function
-    func didChangeFeedArray(_ newFeedArray: [RSSFeed]) {
-        feedArray = newFeedArray
-        getStuff()
+    func didChange(to newFeedArray: [RSSFeed]) {
+        addressArray = newFeedArray.flatMap { $0.rawLink!.absoluteString }
+        print("addressArray:", addressArray)
+        loadFeeds()
         self.tableView.reloadData()
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        var x = 0
-        for feed in feedArray {
-            print("feed #\(x): \(feed.title!)")
-            x += 1
-        }
+        super.viewDidAppear(animated)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        initGetStuff()
         
-        //intialize pull to refresh
+        loadFeeds()
+        
+        // Intialize pull to refresh
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(TableViewController.refresh(_:)), for: UIControlEvents.valueChanged)
         tableView.addSubview(refreshControl)
         
-        }
+        title = "FeedMe"
+        
+    }
     
     func refresh(_ sender: UIRefreshControl) {
-        getStuff()
-        self.tableView.reloadData()
+        itemArray.removeAll()
+        feedArray.removeAll()
+        loadFeeds()
         sender.endRefreshing()
     }
-
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    //special initial getStuff() function to transfer addressArray starter sites to feedArray
-    func initGetStuff() {
-        
-        //get articles from each feed, place in general array, sort by date, load images if possible
-        for address in self.addressArray {
-            let request: URLRequest = URLRequest(url: address)
-            RSSParser.parseFeedForRequest(request, callback: { (feed, error) in
-                if feed != nil {
-                    feed!.rawLink = address
-                    self.feedArray.append(feed!)
-                for articles in feed!.items {
-                    articles.feedName = feed!.title
-                    if articles.imagesFromItemDescription != [] {
-                        self.getDataFromUrl(articles.imagesFromItemDescription[0], completion: { (data, response, error) -> Void in
-                            if error == nil {
-                                articles.picture = UIImage(data: data!)
-                                DispatchQueue.main.async(execute: { () -> Void in
-                                    self.tableView.reloadData()
-                                })
-                            }
-                        })
-                    }
-                    self.itemArray.append(articles)
-                    self.tableView.reloadData()
-                }
-                self.itemArray.sort { $0.pubDate!.compare($1.pubDate!) == .orderedDescending }
-                }
-            })
-        }
-    }
-    
-    //helper function to load pictures safely if they exist, see above
-    func getDataFromUrl(_ url: URL, completion: @escaping ((_ data: Data?, _ response: URLResponse?, _ error: Error? ) -> Void)) {
-        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+    // Helper function to load pictures safely if they exist, see above
+    func getImageData(from url: URL, completion: @escaping ((_ data: Data?, _ response: URLResponse?, _ error: Error?) -> Void)) {
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
             completion(data, response, error)
-            }) .resume()
+        }.resume()
     }
     
-    //parse all feeds in feedArray, and add them to itemArray for display
-    func getStuff() {
-        itemArray.removeAll() //remove exisitng items so there are no duplicates
-        for eachFeed in self.feedArray {
-            RSSParser.parseFeedForRequest(URLRequest(url: eachFeed.rawLink!), callback: { (feed, error) in
-            if feed != nil {
-                self.customTitle = eachFeed.customTitle ?? eachFeed.title!
-                for articles in feed!.items {
-                    articles.feedName = self.customTitle //sets custom/regular title to feedName if exists
-                    if articles.imagesFromItemDescription != [] {
-                        self.getDataFromUrl(articles.imagesFromItemDescription[0], completion: { (data, response, error) -> Void in
-                            if error == nil {
-                                articles.picture = UIImage(data: data!)
-                                DispatchQueue.main.async(execute: { () -> Void in
-                                    self.tableView.reloadData()
-                                })
+    /// Load feed from URL address
+    func loadFeeds() {
+        
+        for address in addressArray {
+            let request: URLRequest = URLRequest(url: URL(string: address)!)
+            RSSParser.parseFeedForRequest(request) { (feed, error) in
+                if error == nil {
+                    self.parse(feed)
+                } else {
+                    print("[TableViewController] loadFeeds Error:", error!.localizedDescription)
+                }
+            }
+        }
+        
+    }
+    
+    /// Parse the
+    func parse(_ feed: RSSFeed?) {
+        
+        if feed != nil {
+            
+            feedArray.append(feed!)
+            
+            for articles in feed!.items {
+                articles.feedName = feed?.title // sets custom/regular title to feedName if exists
+                if articles.imagesFromItemDescription != [] {
+                    self.getImageData(from: articles.imagesFromItemDescription[0]) { (data, response, error) in
+                        if error == nil {
+                            articles.picture = UIImage(data: data!)
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
                             }
-                        })
-                    }
-                    self.itemArray.append(articles)
                         }
                     }
-                    self.itemArray.sort { $0.pubDate!.compare($1.pubDate!) == .orderedDescending }
-                    self.tableView.reloadData()
-                })
+                }
+                self.itemArray.append(articles)
             }
+            
+            itemArray.sort { $0.pubDate!.compare($1.pubDate!) == .orderedDescending }
+            tableView.reloadData()
+            
+        }
+        
+        else {
+            
+            print("[TableViewController] parse: Feed doesn't exist!")
+            
+        }
+        
     }
-    
-    //initalize table with data
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Article") as! TableViewCell
-        cell.setArticle(itemArray[(indexPath as NSIndexPath).row])
+        cell.setArticle(itemArray[indexPath.row])
         return cell
     }
     
-    //open link in SVC, fade article
+    /// Open link in SVC, fade article
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let link = itemArray[(indexPath as NSIndexPath).row].link
-        let svc = SFSafariViewController(url: link! as URL, entersReaderIfAvailable: true)
-        svc.delegate = self
-        self.present(svc, animated: true, completion: nil)
+        let link = itemArray[indexPath.row].link
+        let safariViewController = SFSafariViewController(url: link!, entersReaderIfAvailable: true)
+        self.present(safariViewController, animated: true, completion: nil)
         tableView.cellForRow(at: indexPath)?.alpha = 0.4
     }
     
-    //dismiss SVC
-    func safariViewControllerDidFinish(_ controller: SFSafariViewController) { controller.dismiss(animated: true, completion: nil) }
-    
-    //transfer feed data to feeds page
+    /// Transfer feed data to feeds page
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "feedList" {
             let destination = segue.destination as! FeedViewController
-            destination.feedArrayInFeedView = feedArray
-            }
+            destination.feedArray = feedArray
         }
+    }
     
     func reload() {
+         // remove exisitng items so there are no duplicates
         self.tableView.reloadData()
     }
-    
-    //CODE IN PROGRESS
-    
-    //initalize slideover options / mark as read, incomplete
-    
-    /*
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool { return true }
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) { }
-    override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
-    var markAsReadOrNot = "Mark As Read"
-    if tableView.cellForRowAtIndexPath(indexPath)!.alpha == 0.4 {
-    markAsReadOrNot = "Mark As Unread"
-    }
-    if tableView.cellForRowAtIndexPath(indexPath)!.alpha == 1.0 {
-    markAsReadOrNot = "Mark As Read"
-    }
-    
-    let markAs = UITableViewRowAction(style: .Normal, title: markAsReadOrNot) { action, index in
-    if markAsReadOrNot == "Mark As Read" {
-    tableView.cellForRowAtIndexPath(indexPath)!.alpha = 0.4
-    }
-    if markAsReadOrNot == "Mark As Unread" {
-    tableView.cellForRowAtIndexPath(indexPath)!.alpha = 1.0
-    }
-    self.tableView.setEditing(false, animated: true)
-    }
-    markAs.backgroundColor = UIColor.blueColor()
-    return [markAs]
-    }
-    */
     
 }
 
